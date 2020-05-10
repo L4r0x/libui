be27fec34f1c8f46f201e6a2d64ea9264f3739a3
Lars Wrenger <lars@wrenger.net>
2020-05-10 18:57:20 +0200

    More cleanup

--- unix/button.c
+++ unix/button.c
@@ -4,7 +4,6 @@
 struct uiButton {
 	uiUnixControl c;
 	GtkWidget *widget;
-	GtkButton *button;
 	void (*onClicked)(uiButton *, void *);
 	void *onClickedData;
 };
@@ -14,23 +13,19 @@ uiUnixControlAllDefaults(uiButton)
 static void onClicked(GtkButton *button, gpointer data)
 {
 	uiButton *b = uiButton(data);
-
-	(*(b->onClicked))(b, b->onClickedData);
-}
-
-static void defaultOnClicked(uiButton *b, void *data)
-{
-	// do nothing
+	if (b->onClicked) {
+		(*(b->onClicked))(b, b->onClickedData);
+	}
 }
 
 char *uiButtonText(uiButton *b)
 {
-	return uiUnixStrdupText(gtk_button_get_label(b->button));
+	return uiUnixStrdupText(gtk_button_get_label(GTK_BUTTON(b->widget)));
 }
 
 void uiButtonSetText(uiButton *b, const char *text)
 {
-	gtk_button_set_label(b->button, text);
+	gtk_button_set_label(GTK_BUTTON(b->widget), text);
 }
 
 void uiButtonOnClicked(uiButton *b, void (*f)(uiButton *, void *), void *data)
@@ -42,14 +37,12 @@ void uiButtonOnClicked(uiButton *b, void (*f)(uiButton *, void *), void *data)
 uiButton *uiNewButton(const char *text)
 {
 	uiButton *b;
-
 	uiUnixNewControl(uiButton, b);
-
 	b->widget = gtk_button_new_with_label(text);
-	b->button = GTK_BUTTON(b->widget);
+	b->onClicked = NULL;
+	b->onClickedData = NULL;
 
 	g_signal_connect(b->widget, "clicked", G_CALLBACK(onClicked), b);
-	uiButtonOnClicked(b, defaultOnClicked, NULL);
 
 	return b;
 }
