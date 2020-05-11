29fd7d7b3d24ca033ec38d4041ebcae1da4a2cee
Lars Wrenger <lars@wrenger.net>
2020-05-11 19:46:29 +0200

    Cleanup table model

--- unix/table.h
+++ unix/table.h
@@ -6,21 +6,24 @@
 #include "uipriv_unix.h"
 
 // tablemodel.c
-#define uiTableModelType (uiTableModel_get_type())
-#define uiTableModel(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), uiTableModelType, uiTableModel))
-#define isuiTableModel(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), uiTableModelType))
-#define uiTableModelClass(class) (G_TYPE_CHECK_CLASS_CAST((class), uiTableModelType, uiTableModelClass))
-#define isuiTableModelClass(class) (G_TYPE_CHECK_CLASS_TYPE((class), uiTableModel))
-#define getuiTableModelClass(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), uiTableModelType, uiTableModelClass))
+// Class of definition of out custom table model
 typedef struct uiTableModelClass uiTableModelClass;
+struct uiTableModelClass {
+	GObjectClass parent_class;
+};
+
+// Our custom table model wrapper
 struct uiTableModel {
 	GObject parent_instance;
 	uiTableModelHandler *mh;
 	void *data;
 };
-struct uiTableModelClass {
-	GObjectClass parent_class;
-};
+
 extern GType uiTableModel_get_type(void);
+
+// Conversion macros
+#define uiTableModelType (uiTableModel_get_type())
+#define uiTableModel(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), uiTableModelType, uiTableModel))
+#define uiTableModelClass(class) (G_TYPE_CHECK_CLASS_CAST((class), uiTableModelType, uiTableModelClass))
 
 #endif
