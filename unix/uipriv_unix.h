c63b0c252d41aee6998deabebfe5e679b845e393
Lars Wrenger <lars@wrenger.net>
2020-05-11 19:11:29 +0200

    Cleaning & refactoring the table on unix

--- unix/uipriv_unix.h
+++ unix/uipriv_unix.h
@@ -1,4 +1,6 @@
-// 22 april 2015
+#ifndef __LIBUI_PRIV_UNIX_H__
+#define __LIBUI_PRIV_UNIX_H__
+
 #define GLIB_VERSION_MIN_REQUIRED GLIB_VERSION_2_40
 #define GLIB_VERSION_MAX_ALLOWED GLIB_VERSION_2_40
 #define GDK_VERSION_MIN_REQUIRED GDK_VERSION_3_10
@@ -56,3 +58,5 @@ extern GtkCellRenderer *uiprivNewCellRendererButton(void);
 // future.c
 extern void uiprivLoadFutures(void);
 extern gboolean uiprivFUTURE_gtk_widget_path_iter_set_object_name(GtkWidgetPath *path, gint pos, const char *name);
+
+#endif
